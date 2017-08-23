04a6ec8a4e5e212698aa8ff0b0cbab8d45187a37
zails23 <zails@yahoo.com>
2017-08-22 21:07:13 -0700

    After Sarah's Magic!

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
+// Find out how far work and faves are from top of page (store in variables to use in function)
+var workOffset = $('.work').offset().top;
+var favesOffset = $('.favorites').offset().top;
+
+
+console.log('The work section is ' + workOffset + ' pixels down the page');
 
 $(window).on('scroll', function () {
-  var distanceScrolled = $(window).scrollTop();
+  var distanceScrolled = $(document).scrollTop();
   console.log(distanceScrolled);
 
-  if ($(document).scrollTop() >= 750) {
+  if (distanceScrolled >= workOffset) {
     $('.projects').addClass('faded-in');
   }
-  if ($(document).scrollTop() >= 2276) {
-    $('.fave-things').addClass('faded-in');
+  if (distanceScrolled >= favesOffset - 200) {
+    $('.favorites').addClass('faded-in');
   }
+});
+
+$(window).on('resize', function () {
+  workOffset = $('.work').offset().top;
+  favesOffset = $('.favorites').offset().top;
 });
 
 
@@ -18,3 +29,21 @@ $(window).on('scroll', function () {
 
 // When a user scrolls to the contact section (.contact)
 //  Slowly fadeIn the section .contact
+
+// Smooth scroll this mother!
+
+// Do it when someone clicks a nav link
+$('nav a').on('click', function(e) {
+  // prevent the standard link operation on click
+  e.preventDefault();
+  // use the href of the link to identify what
+  // section to scroll to
+  var thisTarget = $(this).attr('href');
+  // get that section's top offset
+  var targetOffset = $(thisTarget).offset().top - 59;
+  // use jQuery.animate() to animate the body's
+  // scrollTop to the targetOffest
+  $('body').animate({
+    scrollTop: targetOffset
+  }, 600);
+});
